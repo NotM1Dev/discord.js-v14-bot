23d759d701f5a179275073a04a235829a22e7eef
m1-dev <notm1dev@skiff.com>
2023-12-01 07:54:04 +0200

    Complete part #1

---
+++ src/index.js
@@ -0,0 +1,17 @@
+require("dotenv/config");
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.once("ready", () => {
+  console.log(`${client.user.tag} is online.`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
